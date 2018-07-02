7b3ed9b855f73ab0a63a3263d723bedc88952d28
Thomas Corbin <30200976+ThwCorbin@users.noreply.github.com>
2018-07-02 16:46:03 -0400

    Added Add Option and Remove All options functionality

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,13 +1,86 @@
-'use strict';
+"use strict";
 
-console.log('App.js is running!');
+console.log("App.js is running!");
 
-// JSX - JavaScript XML
-var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App'
-);
-var appRoot = document.getElementById('app');
+var app = {
+  title: "Indecision App",
+  subtitle: "Get it done",
+  options: []
+};
 
-ReactDOM.render(template, appRoot);
+var onFormSubmit = function onFormSubmit(e) {
+  e.preventDefault();
+  var option = e.target.elements.option.value;
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = "";
+    render();
+  }
+};
+
+var removeAll = function removeAll() {
+  // e.preventDefault();
+  app.options = [];
+  render();
+};
+
+var render = function render() {
+  var template = React.createElement(
+    "div",
+    null,
+    React.createElement(
+      "h1",
+      null,
+      app.title
+    ),
+    app.subtitle && React.createElement(
+      "h2",
+      null,
+      app.subtitle
+    ),
+    React.createElement(
+      "h2",
+      null,
+      app.options.length > 0 ? "Here are your options:" : "No options"
+    ),
+    React.createElement(
+      "p",
+      null,
+      app.options.length
+    ),
+    React.createElement(
+      "button",
+      { onClick: removeAll },
+      "Remove All"
+    ),
+    React.createElement(
+      "ol",
+      null,
+      React.createElement(
+        "li",
+        null,
+        "Item one"
+      ),
+      React.createElement(
+        "li",
+        null,
+        "Item two"
+      )
+    ),
+    React.createElement(
+      "form",
+      { onSubmit: onFormSubmit },
+      React.createElement("input", { type: "text", name: "option" }),
+      React.createElement(
+        "button",
+        null,
+        "Add Option"
+      )
+    )
+  );
+  ReactDOM.render(template, appRoot);
+};
+
+var appRoot = document.getElementById("app");
+
+render();
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,7 +1,48 @@
-console.log('App.js is running!');
+console.log("App.js is running!");
 
-// JSX - JavaScript XML
-let template = <h1>Indecision App</h1>;
-let appRoot = document.getElementById('app');
+const app = {
+  title: "Indecision App",
+  subtitle: "Get it done",
+  options: []
+};
 
-ReactDOM.render(template, appRoot);
+const onFormSubmit = (e) => {
+  e.preventDefault();
+  const option = e.target.elements.option.value;
+  if(option) {
+    app.options.push(option);
+    e.target.elements.option.value = "";
+    render();
+  }
+};
+
+const removeAll = () => {
+  // e.preventDefault();
+  app.options = [];
+  render();
+};
+
+const render = () => {
+  const template = (
+    <div>
+    <h1>{app.title}</h1>
+    {app.subtitle && <h2>{app.subtitle}</h2>}
+    <h2>{app.options.length > 0 ? `Here are your options:` : "No options"}</h2>
+    <p>{app.options.length}</p>
+    <button onClick={removeAll}>Remove All</button>
+    <ol>
+    <li>Item one</li>
+    <li>Item two</li>
+    </ol>
+    <form onSubmit={onFormSubmit}>
+    <input type="text" name="option" />
+    <button>Add Option</button>
+    </form>
+    </div>
+  );
+  ReactDOM.render(template, appRoot);
+};
+
+const appRoot = document.getElementById("app");
+
+render();
