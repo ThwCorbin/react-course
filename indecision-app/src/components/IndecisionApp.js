444641189f9ee2db77c21821bc9686c6b1e34bfd
Thomas Corbin <30200976+ThwCorbin@users.noreply.github.com>
2018-07-18 14:54:18 -0400

    Implemented babel plugin transform-class-properties

--- indecision-app/src/components/IndecisionApp.js
+++ indecision-app/src/components/IndecisionApp.js
@@ -6,20 +6,33 @@ import Header from "./Header";
 
 export default class IndecisionApp extends React.Component {
   // The class name must be Capitalized for React to work
-  constructor(props) {
-    super(props);
-    this.handlePick = this.handlePick.bind(this);
-    this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
-    this.handleDeleteOption = this.handleDeleteOption.bind(this);
-    this.handleAddOption = this.handleAddOption.bind(this);
-    this.state = {
-      options: []
-    };
-    // In React components declared as ES6 classes, methods follow
-    // the same semantics as regular ES6 classes. This means that they
-    // don’t automatically bind this to the instance. You’ll have to
-    // explicitly use .bind(this) in the constructor.
-  }
+  state = {
+    options: []
+  };
+  handlePick = () => {
+    const randomNum = Math.floor(Math.random() * this.state.options.length);
+    const option = this.state.options[randomNum];
+    alert(option);
+  };
+  handleDeleteOptions = () => {
+    this.setState(() => ({ options: [] }));
+  };
+  handleDeleteOption = OptionToRemove => {
+    this.setState(prevState => ({
+      options: prevState.options.filter(option => OptionToRemove !== option)
+    }));
+  };
+  handleAddOption = option => {
+    if (!option) {
+      return "Enter valid value to add item";
+    } else if (this.state.options.indexOf(option) > -1) {
+      return "This option already exists";
+    }
+    this.setState(prevState => ({ options: prevState.options.concat(option) }));
+    // Use .concat (not .push) so we do not change the prevState array
+    // Wrap the logic after => in parathesis so that the object can implicitly return
+  };
+
   componentDidMount() {
     try {
       const json = localStorage.getItem("options");
@@ -40,29 +53,7 @@ export default class IndecisionApp extends React.Component {
   componentWillUnmount() {
     console.log("componentWillUnmount");
   }
-  handlePick() {
-    const randomNum = Math.floor(Math.random() * this.state.options.length);
-    const option = this.state.options[randomNum];
-    alert(option);
-  }
-  handleDeleteOptions() {
-    this.setState(() => ({ options: [] }));
-  }
-  handleDeleteOption(OptionToRemove) {
-    this.setState(prevState => ({
-      options: prevState.options.filter(option => OptionToRemove !== option)
-    }));
-  }
-  handleAddOption(option) {
-    if (!option) {
-      return "Enter valid value to add item";
-    } else if (this.state.options.indexOf(option) > -1) {
-      return "This option already exists";
-    }
-    this.setState(prevState => ({ options: prevState.options.concat(option) }));
-    // Use .concat (not .push) so we do not change the prevState array
-    // Wrap the logic after => in parathesis so that the object can implicitly return
-  }
+
   render() {
     const subtitle = "Get on with it!";
 
