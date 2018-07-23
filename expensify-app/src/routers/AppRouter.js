d612ddcbad803ba7e3b11b7826b7fdb615320970
Thomas Corbin <30200976+ThwCorbin@users.noreply.github.com>
2018-07-23 13:47:15 -0400

    Broke out components in individual .js files

--- expensify-app/src/routers/AppRouter.js
+++ expensify-app/src/routers/AppRouter.js
@@ -1,41 +1,11 @@
 import React from "react";
-import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
-
-const ExpenseDashboardPage = () => (
-  <div>This is from my dashboard component!</div>
-);
-
-const AddExpensePage = () => <div>This is from my add expense component!</div>;
-const EditExpensePage = () => (
-  <div>This is from my edit expense component!</div>
-);
-const HelpPage = () => <div>This is from my help component!</div>;
-
-const NotFoundPage = () => (
-  // <Link> provides client-side internal routing to="" or to={}
-  <div>
-    404 <Link to="/">Go home</Link>
-  </div>
-);
-
-const Header = () => (
-  // <NavLink> version of <Link> that adds attributes to the rendered element
-  <header>
-    <h1>Expensify</h1>
-    <NavLink to="/" exact activeClassName="is-active">
-      Dashboard
-    </NavLink>
-    <NavLink to="/create" activeClassName="is-active">
-      Create
-    </NavLink>
-    <NavLink to="/edit" activeClassName="is-active">
-      Edit
-    </NavLink>
-    <NavLink to="/help" activeClassName="is-active">
-      Help
-    </NavLink>
-  </header>
-);
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Header from "../components/Header";
+import ExpenseDashboardPage from "../components/ExpenseDashboardPage";
+import AddExpensePage from "../components/AddExpensePage";
+import EditExpensePage from "../components/EditExpensePage";
+import HelpPage from "../components/HelpPage";
+import NotFoundPage from "../components/NotFoundPage";
 
 const AppRouter = () => (
   // BrowserRouter api only allows one child, thus put routes, etc. in a <div>
