da5e8967b5e7e07ba7ae00da61a00dc87a97ae77
Thomas Corbin <30200976+ThwCorbin@users.noreply.github.com>
2018-07-23 11:56:21 -0400

    Init commit Boilerplate

---
+++ boilerplate/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    path: path.join(__dirname, "public"),
+    filename: "bundle.js"
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        loader: "babel-loader",
+        // test for files ending with .js, then load through babel
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        test: /\.s?css$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  },
+  devtool: "cheap-module-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "public")
+  }
+};
